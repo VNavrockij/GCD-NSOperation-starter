b9a2046aee0112cafc67d59629f3337e07216243
VNavrockij <vitalik.nav@gmail.com>
2022-10-10 15:16:58 +0300

    Initial Commit

---
+++ Swift_Lesson_5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Lesson_5
+//
+//  Created by Vitalii Navrotskyi on 10.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
