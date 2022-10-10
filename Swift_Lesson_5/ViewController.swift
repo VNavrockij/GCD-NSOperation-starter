4706c1e34e45602ec24ca69d39e7034d17a20ed0
VNavrockij <vitalik.nav@gmail.com>
2022-10-10 16:38:38 +0300

    add: serial and conccurent queue

--- Swift_Lesson_5/ViewController.swift
+++ Swift_Lesson_5/ViewController.swift
@@ -8,24 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let queue = DispatchQueue(label: "com.multithreading.test ")
+    // serial queue
+    let serialQueue = DispatchQueue(label: "com.gcd_test.serialqueue")
+    // concurrent queue
+    let concurrentQueue = DispatchQueue(label: "com.concurrent.queue", attributes: .concurrent)
+    
+    func iterations() {
+        for i in 1...3 {
+            print("iteration = \(i)")
+            sleep(1)
+        }
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let queue = DispatchQueue(label: "com.multithreading.test ")
         // add queue
-        queue.async {
-            // heavy duty simulation
-            for i in 1...3 {
-                print("iteration = \(i)")
-                sleep(1)
-            }
-            
-        }
+//        queue.async {
+//            // heavy duty simulation
+//            self.iterations()
+//        }
         
+        concurrentQueue.async {
+            self.iterations()
+        }
+        concurrentQueue.async {
+            self.iterations()
+        }
         
         print("iterations finished ")
     }
+    
 
 
 }
