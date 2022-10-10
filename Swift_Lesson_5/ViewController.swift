8e1e0308a1e501654cd397f609ce9760a2669053
VNavrockij <vitalik.nav@gmail.com>
2022-10-10 16:56:50 +0300

    add: NSOperation, NSOperationQueue

--- Swift_Lesson_5/ViewController.swift
+++ Swift_Lesson_5/ViewController.swift
@@ -6,9 +6,34 @@
 //
 
 import UIKit
+/* NSOperation, NSOperationQueue */
+class LongRunOperation: Operation {
+    
+    let identifier: String
+    
+    init(identifier: String) {
+        self.identifier = identifier
+    }
+    
+    override func main() {
+        if isCancelled {
+            return
+        }
+        iteration()
+    }
+    
+    func iteration() {
+            for count in 1...3 {
+                print("\(identifier) working \(count)")
+                sleep(1)
+            }
+            print("\(identifier) finished")
+        }
+}
 
 class ViewController: UIViewController {
     
+/* GCD
     let queue = DispatchQueue(label: "com.multithreading.test ")
     // serial queue
     let serialQueue = DispatchQueue(label: "com.gcd_test.serialqueue")
@@ -21,12 +46,17 @@ class ViewController: UIViewController {
             sleep(1)
         }
     }
+ */
+/* NSOperation, NSOperationQueue */
+    let test1 = LongRunOperation(identifier: "test1")
+    let test2 = LongRunOperation(identifier: "test2")
+    let test3 = LongRunOperation(identifier: "test3")
     
-    
+    let oparationsQueue = OperationQueue()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+/* GCD
         // add queue
 //        queue.async {
 //            // heavy duty simulation
@@ -41,6 +71,10 @@ class ViewController: UIViewController {
         }
         
         print("iterations finished ")
+*/
+/* NSOperation, NSOperationQueue */
+        oparationsQueue.addOperations([test1, test2, test3], waitUntilFinished: false)
+        
     }
     
 
